03e2ca22143b083c8d7ac5e7d45931d6c68eab9d
Aleksandryia Karyshava <alexandria@AleksanryiasMBP.localdomain>
2026-02-25 13:38:03 -0800

    expand config file

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.saucedemo.com/",
+    retries: process.env.CI ? { runMode: 2, openMode: 0 } : { runMode: 1, openMode: 0 },
+    defaultCommandTimeout: process.env.CI ? 10000 : 4000,
+    pageLoadTimeout: process.env.CI ? 120000 : 60000,
     specPattern: "cypress/e2e/**/*.cy.js",
     supportFile: "cypress/support/e2e.js",
     viewportWidth: 1280,
